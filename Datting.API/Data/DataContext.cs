366dc5a9874e536050c09ee8bc57447d04bb3ee0
wagnergarnizet <61944236+wagnergarnizet@users.noreply.github.com>
2020-04-26 19:54:26 -0300

    APP

---
+++ Datting.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Datting.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datting.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+        public DbSet<Value> Values   { get; set; }
+    }
+}
