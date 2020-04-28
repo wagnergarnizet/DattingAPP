0d9f3b6a47cd6dacfdc14a127034c1fbb0261043
wagnergarnizet <61944236+wagnergarnizet@users.noreply.github.com>
2020-04-27 21:13:01 -0300

    APP

--- Datting.API/Data/DataContext.cs
+++ Datting.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace Datting.API.Data
         {
         }
         public DbSet<Value> Values   { get; set; }
+        public DbSet<User> Users {get;set;}
     }
 }
--- Datting.API/Migrations/20200427225628_addeduserentity.Designer.cs
+++ Datting.API/Migrations/20200427225628_addeduserentity.Designer.cs
@@ -4,14 +4,16 @@ using Datting.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Datting.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200427225628_addeduserentity")]
+    partial class addeduserentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
