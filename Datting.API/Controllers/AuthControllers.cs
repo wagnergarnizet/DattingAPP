5e4de3a76ba67e373dcf5c8c13b5a046e4c27989
wagnergarnizet <61944236+wagnergarnizet@users.noreply.github.com>
2020-04-27 22:23:09 -0300

    APP

---
+++ Datting.API/Controllers/AuthControllers.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Datting.API.Data;
+using System.Threading.Tasks;
+using Datting.API.Models;
+using Datting.API.Dtos;
+
+namespace Datting.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthControllers: ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthControllers(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            //validação do request
+
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+
+            if(await _repo.UserExists(userForRegisterDto.UserName))
+                return BadRequest("Usuário já cadastrado!");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.UserName
+            };
+
+            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.Password);
+
+            return StatusCode(201);
+
+
+        }
+
+    }
+}
---
+++ Datting.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace Datting.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string UserName { get; set; }    
+        public string Password { get; set; }
+    }
+}
